10900ba5b816ec7875134ed18e77a606e4838e1c
Marc Portier <marc.portier@gmail.com>
2022-07-29 17:50:24 +0200

    new test

---
+++ tests/test_cli.py
@@ -0,0 +1,16 @@
+import pytest
+import sys
+import os
+from util4tests import enable_test_logging, run_single_test, log
+
+from pykg2tbl import KG2TblService, KGFileSource, KG2EndpointSource, J2SparqlBuilder
+from pykg2tbl.__main__ import main as kg2tbl
+
+
+
+def test_basic():
+    kg2tbl('-i tests/sources/01-persons-shape.ttl tests/sources/02-person.ttl -o /tmp/test_kg2tbl_vliz.csv -tf tests/sparql_templates -t all.sparql'.split(' '))
+    pass
+
+if __name__ == "__main__":
+    run_single_test(__file__)
