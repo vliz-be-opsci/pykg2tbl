263257bec009200bb47bee7d9bd2ec2d7859ba4c
Marc Portier <marc.portier@gmail.com>
2022-07-29 17:48:44 +0200

    readability and spelling

--- tests/test_j2sparql.py
+++ tests/test_j2sparql.py
@@ -10,18 +10,18 @@ from pykg2tbl import KG2TblService, KGFileSource, KG2EndpointSource, J2SparqlBui
 ALL_TRIPLES_SPARQL = "SELECT * WHERE { ?s ?p ?o. } LIMIT 10"
 BODC_ENDPOINT = "http://vocab.nerc.ac.uk/sparql/sparql"
 
-
+ALL_QUERY="""SELECT * 
+WHERE { 
+    ?s ?p ?o. 
+} 
+LIMIT 100"""
 class TestBuilder(unittest.TestCase):
     def test_basic_query_sparql(self):
         template_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'sparql_templates')
         j2sqb = J2SparqlBuilder(template_folder)
         qry = j2sqb.build_sparql_query("all.sparql")
         self.assertIsNotNone(qry, "result qry should exist")
-        self.assertEqual('''SELECT * 
-WHERE { 
-    ?s ?p ?o. 
-} 
-LIMIT 10''',qry,'unexpected qry result')
+        self.assertEqual(ALL_QUERY, qry, 'unexpected qry result')
 
     def test_get_variables_sparql_query(self):
         #TODO write test to get all the variables from a sparql template
@@ -32,7 +32,7 @@ LIMIT 10''',qry,'unexpected qry result')
         log.info(f"all variables {variables}")
         self.assertIsNotNone(variables,'variables should exist')
         
-    def test_injested_query_sparql(self):
+    def test_ingested_query_sparql(self):
         #test a sparql template who uses variables to make a sparql query to see if it works
         
         pass
