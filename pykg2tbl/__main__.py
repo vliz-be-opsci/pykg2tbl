249cb999347981ae179b5f4d715f8613d240be82
JotaFan <jotaflame@gmail.com>
2023-05-11 10:24:22 +0200

    chore: apply liting suggestions

--- pykg2tbl/__main__.py
+++ pykg2tbl/__main__.py
@@ -1,92 +1,102 @@
 # -*- coding: utf-8 -*-
 import argparse
-import sys
-import os
 import logging
 import logging.config
+import os
+import sys
+
 import validators
-from pykg2tbl import KG2TblService, KGFileSource, KG2EndpointSource, J2SparqlBuilder
+
+from pykg2tbl import (
+    J2SparqlBuilder,
+    KG2EndpointSource,
+    KG2TblService,
+    KGFileSource,
+)
 
 log = logging.getLogger(__name__)
+
 
 def get_arg_parser():
     """
     Defines the arguments to this script by using Python's [argparse](https://docs.python.org/3/library/argparse.html)
     """
-    parser = argparse.ArgumentParser(description='Py Project to extra table data from knowwledge-graphs using sparql templates',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument(
-        '-l',
-        '--logconf',
-        type=str,
-        action='store',
-        help='location of the logging config (yml) to use',
+    parser = argparse.ArgumentParser(
+        description="Py Project to extra table data from knowwledge-graphs using sparql templates",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
     parser.add_argument(
-        '-i',
-        '--input',
+        "-l",
+        "--logconf",
         type=str,
-        nargs='+',
+        action="store",
+        help="location of the logging config (yml) to use",
+    )
+
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        nargs="+",
         metavar="FILE",
-        action='store',
-        help='input file to be turned into datagraph or endpoint of rdf-database'
+        action="store",
+        help="input file to be turned into datagraph or endpoint of rdf-database",
     )
 
     parser.add_argument(
-        '-e',
-        '--endpoint',
+        "-e",
+        "--endpoint",
         type=str,
         metavar="URL",
-        action='store',
-        help='sparql endpoint url'
+        action="store",
+        help="sparql endpoint url",
     )
 
     parser.add_argument(
-        '-f',
-        '--output_format',
-        choices=['csv','tsv'],
-        action='store',
-        help='output file format',
-        default='csv'
+        "-f",
+        "--output_format",
+        choices=["csv", "tsv"],
+        action="store",
+        help="output file format",
+        default="csv",
     )
 
     parser.add_argument(
-        '-o',
-        '--output_location',
+        "-o",
+        "--output_location",
         type=str,
         metavar="FILE",
-        action='store',
-        help='output file location relative to current directory path',
+        action="store",
+        help="output file location relative to current directory path",
     )
 
     parser.add_argument(
-        '-tf',
-        '--template_folder',
+        "-tf",
+        "--template_folder",
         type=str,
         metavar="PATH",
-        action='store',
-        help='template folder location on disk',
+        action="store",
+        help="template folder location on disk",
     )
 
     parser.add_argument(
-        '-t',
-        '--template_name',
+        "-t",
+        "--template_name",
         type=str,
-        action='store',
-        help='template name located in the given template folder',
+        action="store",
+        help="template name located in the given template folder",
     )
 
     # TODO -- unclear how / why this approach?
     # where to add actual vars vs. subcommand to list vars?
     parser.add_argument(
-        '-v',
-        '--variables',
-        nargs='*',
-        action='append',
-        help='List the variable names required for the given template seperated by |'
-        )
+        "-v",
+        "--variables",
+        nargs="*",
+        action="append",
+        help="List the variable names required for the given template seperated by |",
+    )
 
     return parser
 
@@ -94,43 +104,59 @@ def get_arg_parser():
 def enable_logging(args: argparse.Namespace):
     if args.logconf is None:
         return
-    import yaml   # conditional dependency -- we only need this (for now) when logconf needs to be read
-    with open(args.logconf, 'r') as yml_logconf:
-        logging.config.dictConfig(yaml.load(yml_logconf, Loader=yaml.SafeLoader))
+    import yaml  # conditional dependency -- we only need this (for now) when logconf needs to be read
+
+    with open(args.logconf, "r") as yml_logconf:
+        logging.config.dictConfig(
+            yaml.load(yml_logconf, Loader=yaml.SafeLoader)
+        )
     log.info(f"Logging enabled according to config in {args.logconf}")
+
 
 def performe_service(args: argparse.Namespace):
     # check if all necessary variables are given
-    # TODO - why do this in __main__ ?? careful considertation --> either (1) remove, (2) move to service or (3) motivate and keep here in cli 
+    # TODO - why do this in __main__ ?? careful considertation --> either (1) remove, (2) move to service or (3) motivate and keep here in cli
     if args.input is not None and args.endpoint is not None:
-        raise argparse.ArgumentTypeError('Either a fileinput or an endpoint must be supplied, not both.')
+        raise argparse.ArgumentTypeError(
+            "Either a fileinput or an endpoint must be supplied, not both."
+        )
     if args.input is None and args.endpoint is None:
-        raise argparse.ArgumentTypeError('A fileinput or an endpoint must be supplied.')
+        raise argparse.ArgumentTypeError(
+            "A fileinput or an endpoint must be supplied."
+        )
     if args.template_name is None:
-        raise argparse.ArgumentTypeError('A template name must be supplied.')
+        raise argparse.ArgumentTypeError("A template name must be supplied.")
     if args.output_location is None:
-        raise argparse.ArgumentTypeError('An output location must be supplied.')
+        raise argparse.ArgumentTypeError(
+            "An output location must be supplied."
+        )
 
-    #per variable check if they are valid for consumption
-    # TODO -- why? 
+    # per variable check if they are valid for consumption
+    # TODO -- why?
     current_folder = os.getcwd()
     if args.input is not None:
         for i in args.input:
-            log.debug(os.path.join(current_folder,i))
-            if os.path.exists(os.path.join(current_folder,i)) == False:
-                raise argparse.ArgumentTypeError(f'file {i} does exist')
+            log.debug(os.path.join(current_folder, i))
+            if os.path.exists(os.path.join(current_folder, i)) is False:
+                raise argparse.ArgumentTypeError(f"file {i} does exist")
 
     # TODO why? why here and not in service?
     if args.endpoint is not None:
-        if validators.url(args.endpoint) !=True:
-            raise argparse.ArgumentTypeError(f'given endpoint is not a valid url => {args.endpoint} ')
+        if validators.url(args.endpoint) is not True:
+            raise argparse.ArgumentTypeError(
+                f"given endpoint is not a valid url => {args.endpoint} "
+            )
 
-    #check if output path exists
+    # check if output path exists
     if args.output_location is not None:
-        folder_path_file = os.path.dirname(os.path.abspath(os.path.join(os.getcwd(),args.output_location)))
+        folder_path_file = os.path.dirname(
+            os.path.abspath(os.path.join(os.getcwd(), args.output_location))
+        )
         log.debug(folder_path_file)
-        if os.path.exists(folder_path_file) == False:
-            raise argparse.ArgumentTypeError('Supplied output path does not exist on disk.')
+        if os.path.exists(folder_path_file) is False:
+            raise argparse.ArgumentTypeError(
+                "Supplied output path does not exist on disk."
+            )
 
 
 def args_values_to_params(argv_list: list) -> dict:
@@ -150,44 +176,59 @@ def args_values_to_params(argv_list: list) -> dict:
     for lin in argv_list:
         log.debug(lin)
         line = str(lin)
-        key, value = line.split('=')
-        if '.' in key:
-            parts = key.split('.')
-            assert len(parts) == 2, "dict-value key '%s' does not match the single level support provided" % key
+        key, value = line.split("=")
+        if "." in key:
+            parts = key.split(".")
+            assert len(parts) == 2, (
+                "dict-value key '%s' does not match the single level support provided"
+                % key
+            )
             key, subkey = parts[0], parts[1]
             subdict = params.get(key, dict())
-            assert isinstance(subdict, dict), "dict-value key '%s' is also used for none-dict value" % key
+            assert isinstance(subdict, dict), (
+                "dict-value key '%s' is also used for none-dict value" % key
+            )
             subdict[subkey] = value
             params[key] = subdict
-        elif key.endswith('[]'):
+        elif key.endswith("[]"):
             key = key[:-2]
             sublist = params.get(key, list())
-            assert isinstance(sublist, list), "list-value key '%s' is also used for none-list value" % key
-            values = value.split(',')
+            assert isinstance(sublist, list), (
+                "list-value key '%s' is also used for none-list value" % key
+            )
+            values = value.split(",")
             sublist.extend(values)
             params[key] = sublist
         else:
-            assert key not in params, "single-value key '%s' should not be set twice" % key
+            assert key not in params, (
+                "single-value key '%s' should not be set twice" % key
+            )
             params[key] = value
     return params
+
 
 def variables_check(variables_template, variables_given):
     for variable in variables_template:
         intemplate = False
         for var in variables_given.keys():
-            if var  == variable:
+            if var == variable:
                 intemplate = True
-        if intemplate == False:
-            raise argparse.ArgumentTypeError(f'variable {variable} is not present in template')
+        if intemplate is False:
+            raise argparse.ArgumentTypeError(
+                f"variable {variable} is not present in template"
+            )
 
     for variable in variables_given.keys():
         intemplate = False
         log.debug(variable)
         for var in variables_template:
-            if var  == variable:
+            if var == variable:
                 intemplate = True
-        if intemplate == False:
-            raise argparse.ArgumentTypeError(f'variable {variable} is not present in template')
+        if intemplate is False:
+            raise argparse.ArgumentTypeError(
+                f"variable {variable} is not present in template"
+            )
+
 
 def makesource(args: argparse.Namespace):
     if args.input is not None:
@@ -195,23 +236,29 @@ def makesource(args: argparse.Namespace):
     if args.endpoint is not None:
         return KG2EndpointSource(args.endpoint)
 
+
 def getdelimiter(args: argparse.Namespace):
     if args.output_format is not None:
         if args.output_format == "csv":
-            return ','
+            return ","
         if args.output_format == "tsv":
-            return '\t'
+            return "\t"
     else:
-        return ','
+        return ","
 
-def main(sysargs = None):
+
+def main(sysargs=None):
     """
     The main entry point to this module.
 
     """
     # TODO use log instead
-    print('sysargs=', sysargs)
-    args = get_arg_parser().parse_args(sysargs) if sysargs is not None and len(sysargs) > 0 else get_arg_parser().parse_args()
+    print("sysargs=", sysargs)
+    args = (
+        get_arg_parser().parse_args(sysargs)
+        if sysargs is not None and len(sysargs) > 0
+        else get_arg_parser().parse_args()
+    )
     enable_logging(args)
     log.info("The args passed to %s are: %s." % (sys.argv[0], args))
     log.debug("Performing service")
@@ -221,18 +268,28 @@ def main(sysargs = None):
     vars_template = template_service.variables_in_query(args.template_name)
     if args.variables is not None and len(vars_template) > 0:
         params = args_values_to_params(args.variables)
-        variables_check(variables_template=vars_template, variables_given=params)
-    querry = template_service.build_sparql_query(name=args.template_name, variables= params)
-    print(f'Making KGSource')
+        variables_check(
+            variables_template=vars_template, variables_given=params
+        )
+    querry = template_service.build_sparql_query(
+        name=args.template_name, variables=params
+    )
+    print("Making KGSource")
     source = makesource(args)
-    print(f'performing query')
+    print("performing query")
     log.debug("making exec service")
     executive_service = KG2TblService(source)
     log.debug("performing service query")
-    executive_service.exec(querry,os.path.join(os.getcwd(),args.output_location),getdelimiter(args))
+    executive_service.exec(
+        querry,
+        os.path.join(os.getcwd(), args.output_location),
+        getdelimiter(args),
+    )
     log.info("done with query")
-    print(f'new file saved on location : {os.path.join(os.getcwd(),args.output_location)}')
+    print(
+        f"new file saved on location : {os.path.join(os.getcwd(),args.output_location)}"
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1:])
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,10 +1,8 @@
-import pytest
-import sys
 import os
-from util4tests import enable_test_logging, run_single_test, log
 
-from pykg2tbl import KG2TblService, KGFileSource, KG2EndpointSource, J2SparqlBuilder
+from util4tests import log, run_single_test
 
+from pykg2tbl import J2SparqlBuilder, KG2EndpointSource, KGFileSource
 
 ALL_TRIPLES_SPARQL = "SELECT * WHERE { ?s ?p ?o. } LIMIT 25"
 # TODO provide some registry of endpoints to choose from --> issue #4 -- then replace next line!
@@ -12,39 +10,45 @@ BODC_ENDPOINT = "http://vocab.nerc.ac.uk/sparql/sparql"
 
 
 def test_basic_filesource():
-    file_base = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'sources')
+    file_base = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), "sources"
+    )
     log.debug(f"test using files in {file_base}")
 
-    # TODO provide better input files to test with 
-    test_source  = KGFileSource(os.path.join(file_base, '02-person.ttl'),
-                                os.path.join(file_base, '01-persons-shape.ttl'))
+    # TODO provide better input files to test with
+    test_source = KGFileSource(
+        os.path.join(file_base, "02-person.ttl"),
+        os.path.join(file_base, "01-persons-shape.ttl"),
+    )
     result = test_source.query(ALL_TRIPLES_SPARQL)
     log.debug(result)
     assert result is not None, "result should exist"
     log.debug(f"result ==> {result}")
 
     # TODO some decent result iteration / representation / dataframe?
-    # allow it to assert 
-    #   -  length of result 
+    # allow it to assert
+    #   -  length of result
     #   -  content being present or not
 
 
 def test_basic_endpoint():
-    test_source  = KG2EndpointSource(BODC_ENDPOINT)
+    test_source = KG2EndpointSource(BODC_ENDPOINT)
     result = test_source.query(ALL_TRIPLES_SPARQL)
-    #log.debug(result)
+    # log.debug(result)
     assert result is not None, "result should exist"
     # TODO more elaborate assertions
-    
+
 
 def test_full_search():
-    #make full search on the endpoint of BODC to see what it returns // test on the BODC server itself first
-    test_source  = KG2EndpointSource(BODC_ENDPOINT)
-    #make test qry using template from BODC
+    # make full search on the endpoint of BODC to see what it returns // test on the BODC server itself first
+    test_source = KG2EndpointSource(BODC_ENDPOINT)
+    # make test qry using template from BODC
     log.info("full test")
     j2sqb = J2SparqlBuilder()
 
-    qry = j2sqb.build_sparql_query("bodc-find.sparql", collections=['P01'], regex=".*orca.*")
+    qry = j2sqb.build_sparql_query(
+        "bodc-find.sparql", collections=["P01"], regex=".*orca.*"
+    )
     log.debug(f"query = {qry}")
     result = test_source.query(qry)
     log.debug(f"result = {result}")
