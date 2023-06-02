5c3377905d5d53417482d5aec9e390d08e7efecc
JotaFan <jotaflame@gmail.com>
2023-06-02 09:10:44 +0200

    fix: fixes on comented remarks

--- tests/j2/const.py
+++ tests/j2/const.py
@@ -1,8 +1,8 @@
 from jinja2 import Environment, FileSystemLoader
 
-from pykg2tbl.j2.jinja_sparql_builder import TEMPLATES_FOLDER
+from pykg2tbl.j2.jinja_sparql_builder import DEFAULT_TEMPLATES_FOLDER
 
-file_system_loader = FileSystemLoader(TEMPLATES_FOLDER)
+file_system_loader = FileSystemLoader(DEFAULT_TEMPLATES_FOLDER)
 templates_env = Environment(loader=file_system_loader)
 
 sparql_templates_list = templates_env.list_templates()
@@ -10,7 +10,7 @@ sparql_templates_list = templates_env.list_templates()
 template_variables = {
     "all.sparql": set({"N"}),
     "bodc-find.sparql": set({"regex", "collections", "language"}),
-    "broader-terms.sparql": set({"triple", "language"}),
+    "broader-terms.sparql": set({"term", "language"}),
     "rdf-predicates-count.sparql": set(),
     "rdf-predicates.sparql": set({"regex"}),
     "rdf-types.sparql": set({"regex"}),
