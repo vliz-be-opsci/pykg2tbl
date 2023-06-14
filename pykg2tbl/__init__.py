24a73a2cf814f34636e0f0ddfc327cca78a7a09f
JotaFan <jotaflame@gmail.com>
2023-06-14 09:39:48 +0200

    chore: remove jinja folder adding pyrdfj2 as dependency

--- pykg2tbl/__init__.py
+++ pykg2tbl/__init__.py
@@ -9,13 +9,16 @@
 """
 
 import logging
+from pathlib import Path
 
-from pykg2tbl.j2.jinja_sparql_builder import (
-    J2SparqlBuilder as DefaultSparqlBuilder,
-)
+from pyrdfj2 import J2RDFSyntaxBuilder as DefaultSparqlBuilder
+
 from pykg2tbl.kg2tbl import KGSource
 from pykg2tbl.query import QueryResult
 
 log = logging.getLogger(__name__)
+DEFAULT_TEMPLATES_FOLDER = (
+    Path(__file__).parent.absolute() / "sparql_templates"
+)
 
 __all__ = ["DefaultSparqlBuilder", "KGSource", "QueryResult"]
